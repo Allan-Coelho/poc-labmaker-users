2a1a714554ced77d3c63590d8d1b783317e0e0b9
Allan Vieira Coelho <github@allanvieira.dev>
2022-11-14 13:59:30 -0300

    feat(database/dump): database dump file

--- dump.sql
+++ dump.sql
@@ -1,41 +1,307 @@
-CREATE TABLE "users" (
-	"id" serial NOT NULL,
-	"role_id" serial NOT NULL,
-	"permission_id" integer NOT NULL,
-	"institutional_email" TEXT NOT NULL UNIQUE,
-	"personal_email" TEXT NOT NULL UNIQUE,
-	"name" TEXT NOT NULL,
-	"phone" TEXT NOT NULL UNIQUE,
-	"cep" TEXT NOT NULL,
-	"address_number" TEXT NOT NULL,
-	"address_additional_information" TEXT NOT NULL,
-	"institutional_id" TEXT NOT NULL UNIQUE,
-	CONSTRAINT "users_pk" PRIMARY KEY ("id")
-) WITH (OIDS = FALSE);
+--
+-- PostgreSQL database dump
+--
 
-CREATE TABLE "roles" (
-	"id" serial NOT NULL,
-	"name" TEXT NOT NULL,
-	CONSTRAINT "roles_pk" PRIMARY KEY ("id")
-) WITH (OIDS = FALSE);
+-- Dumped from database version 14.5 (Ubuntu 14.5-0ubuntu0.22.04.1)
+-- Dumped by pg_dump version 14.5 (Ubuntu 14.5-0ubuntu0.22.04.1)
 
-CREATE TABLE "permissions" (
-	"id" serial NOT NULL,
-	"name" TEXT NOT NULL,
-	"can_borrow_assets" BOOLEAN NOT NULL,
-	"can_shop" BOOLEAN NOT NULL,
-	"is_laboratory_admin" BOOLEAN NOT NULL,
-	"is_institutional_employee" BOOLEAN NOT NULL,
-	"is_laboratory_staff" BOOLEAN NOT NULL,
-	CONSTRAINT "permissions_pk" PRIMARY KEY ("id")
-) WITH (OIDS = FALSE);
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
 
-ALTER TABLE
-	"users"
-ADD
-	CONSTRAINT "users_fk0" FOREIGN KEY ("role_id") REFERENCES "roles"("id");
+SET default_tablespace = '';
 
-ALTER TABLE
-	"users"
-ADD
-	CONSTRAINT "users_fk1" FOREIGN KEY ("permission_id") REFERENCES "permissions"("id");
+SET default_table_access_method = heap;
+
+--
+-- Name: permissions; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.permissions (
+    id integer NOT NULL,
+    name text NOT NULL,
+    can_borrow_assets boolean NOT NULL,
+    can_shop boolean NOT NULL,
+    is_laboratory_admin boolean NOT NULL,
+    is_institutional_employee boolean NOT NULL,
+    is_laboratory_staff boolean NOT NULL
+);
+
+
+--
+-- Name: permissions_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.permissions_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: permissions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.permissions_id_seq OWNED BY public.permissions.id;
+
+
+--
+-- Name: roles; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.roles (
+    id integer NOT NULL,
+    name text NOT NULL
+);
+
+
+--
+-- Name: roles_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.roles_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: roles_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.roles_id_seq OWNED BY public.roles.id;
+
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    role_id integer NOT NULL,
+    permission_id integer NOT NULL,
+    institutional_email text NOT NULL,
+    personal_email text NOT NULL,
+    name text NOT NULL,
+    phone text NOT NULL,
+    cep text NOT NULL,
+    address_number text NOT NULL,
+    address_additional_information text NOT NULL,
+    institutional_id text NOT NULL
+);
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.users_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+
+
+--
+-- Name: users_role_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.users_role_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: users_role_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.users_role_id_seq OWNED BY public.users.role_id;
+
+
+--
+-- Name: permissions id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.permissions ALTER COLUMN id SET DEFAULT nextval('public.permissions_id_seq'::regclass);
+
+
+--
+-- Name: roles id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.roles ALTER COLUMN id SET DEFAULT nextval('public.roles_id_seq'::regclass);
+
+
+--
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Name: users role_id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN role_id SET DEFAULT nextval('public.users_role_id_seq'::regclass);
+
+
+--
+-- Data for Name: permissions; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+INSERT INTO public.permissions VALUES (1, 'standard', true, true, false, false, false);
+INSERT INTO public.permissions VALUES (2, 'teacher', true, true, false, true, false);
+INSERT INTO public.permissions VALUES (3, 'staff', true, true, true, false, true);
+INSERT INTO public.permissions VALUES (4, 'technician', true, true, true, true, false);
+
+
+--
+-- Data for Name: roles; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+INSERT INTO public.roles VALUES (1, 'Professor(a) Titular');
+INSERT INTO public.roles VALUES (2, 'Monitor(a)');
+INSERT INTO public.roles VALUES (3, 'Voluntário(a)');
+INSERT INTO public.roles VALUES (4, 'Aluno(a) de Graduação');
+INSERT INTO public.roles VALUES (5, 'Aluno(a) de pós-graduação');
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+INSERT INTO public.users VALUES (7, 4, 1, 'allessandro@usp.br', 'allessandronqq@gmail.com', 'Allessandro Santana', '+55 16 78945-6123', '13561-060', '100', 'apto 10', '147951951');
+INSERT INTO public.users VALUES (5, 4, 3, 'allan@usp.br', 'allan@gmail.com', 'Allan Vieira Coelho', '+55 16 99405-6289', '13561-060', '1057', 'apto 1', '15915915');
+
+
+--
+-- Name: permissions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.permissions_id_seq', 4, true);
+
+
+--
+-- Name: roles_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.roles_id_seq', 5, true);
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.users_id_seq', 7, true);
+
+
+--
+-- Name: users_role_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.users_role_id_seq', 1, false);
+
+
+--
+-- Name: permissions permissions_pk; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.permissions
+    ADD CONSTRAINT permissions_pk PRIMARY KEY (id);
+
+
+--
+-- Name: roles roles_pk; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.roles
+    ADD CONSTRAINT roles_pk PRIMARY KEY (id);
+
+
+--
+-- Name: users users_institutional_email_key; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_institutional_email_key UNIQUE (institutional_email);
+
+
+--
+-- Name: users users_institutional_id_key; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_institutional_id_key UNIQUE (institutional_id);
+
+
+--
+-- Name: users users_personal_email_key; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_personal_email_key UNIQUE (personal_email);
+
+
+--
+-- Name: users users_phone_key; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_phone_key UNIQUE (phone);
+
+
+--
+-- Name: users users_pk; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pk PRIMARY KEY (id);
+
+
+--
+-- Name: users users_fk0; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_fk0 FOREIGN KEY (role_id) REFERENCES public.roles(id);
+
+
+--
+-- Name: users users_fk1; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_fk1 FOREIGN KEY (permission_id) REFERENCES public.permissions(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
